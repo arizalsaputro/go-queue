10dca2a57d2ee80fc89242d2b576890f3a83f2c9
Arizal <muhammadarizals1@gmail.com>
2025-02-22 17:21:02 +0700

    chore: update example

--- example/kq/consumer/queue.go
+++ example/kq/consumer/queue.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-
 	"github.com/zeromicro/go-queue/kq"
 	"github.com/zeromicro/go-zero/core/conf"
 )
@@ -12,8 +11,8 @@ func main() {
 	var c kq.KqConf
 	conf.MustLoad("config.yaml", &c)
 
-	q := kq.MustNewQueue(c, kq.WithHandle(func(ctx context.Context, k, v string) error {
-		fmt.Printf("=> %s\n", v)
+	q := kq.MustNewQueue(c, kq.WithHandle(func(ctx context.Context, key string, value []byte) error {
+		fmt.Printf("%s => %s\n", key, value)
 		return nil
 	}))
 	defer q.Stop()
